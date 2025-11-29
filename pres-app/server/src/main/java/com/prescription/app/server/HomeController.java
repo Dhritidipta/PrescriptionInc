e8185a74a95ce6cfaa2a444df89d7ea4a260148d
Dhritidipta <61513692+Dhritidipta@users.noreply.github.com>
2025-11-30 00:34:37 +0530

    refactor I

---
+++ pres-app/server/src/main/java/com/prescription/app/server/HomeController.java
@@ -0,0 +1,19 @@
+package com.prescription.app.server;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "redirect:https://prescription-inc.vercel.app/";
+    }
+
+    // @GetMapping("/list")
+    // public String PresListController(){
+    //     return "Here is the list of Prescriptions";
+    // }
+}
